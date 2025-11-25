8ede1dfdd66100cb66507c3c7fc8cd46f0a63896
c0a24041 <c0a24041ca@edu.teu.ac.jp>
2025-11-25 14:36:33 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,17 @@ DELTA = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+
+def check_bound(rct:pg.Rect):
+    yoko,tate = True,True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko,tate
+
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -51,7 +62,14 @@ def main():
                 sum_mv[0] += mv[0] #縦方向
                 sum_mv[1] += mv[1] #横方向
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) !=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
